57885cabb5ec5465a1db2a1a583290e86daadc8c
NeuroResist <pataponchik3@gmail.com>
2023-03-19 13:14:00 +0300

    feat: добавлен key, мелкие исправления

--- src/pages/SideMenu/ModalHelpSections/HelpSection.tsx
+++ src/pages/SideMenu/ModalHelpSections/HelpSection.tsx
@@ -1,22 +1,23 @@
 import { ReactElement } from "react";
 import clsx from "clsx";
+
 import { IHelpSection } from "./interface";
 
 function HelpSection(props: IHelpSection) {
   const { header, paragraphs, styles } = props;
 
   return (
-    <>
+    <article>
       <h3 className={clsx("font-bold text-pink text-2xl mb-2 ", styles?.header)}>{header}</h3>
 
       <ul className="mb-3 [&>*]:mb-2 pb-2 border-b-2 border-b-black last:border-none">
-        {paragraphs.map((paragraph: ReactElement) => (
-          <li className={clsx("last:mb-0", styles?.paragraph)}>
+        {paragraphs.map((paragraph: ReactElement, i) => (
+          <li key={`Заголовок_${i}`} className={clsx("last:mb-0", styles?.paragraph)}>
             <p>{paragraph}</p>
           </li>
         ))}
       </ul>
-    </>
+    </article>
   );
 }
 
